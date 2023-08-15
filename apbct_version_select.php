24c98eb08d18c8e0cda4dee23c5d27b5c7f3b4ca
alexandergull <galyshev@cleantalk.org>
2023-08-15 14:12:54 +0500

    Somethings works.

--- apbct_version_select.php
+++ apbct_version_select.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Plugin Name:       APBCT plugin versions selector
+ * Plugin Name:       CleanTalk plugins custom version installer
  * Plugin URI:        https://cleantalk.org
  * Description:       Handle the basics with this plugin.
  * Version:           1.0
@@ -20,57 +20,97 @@
 define('AVS_VERSIONS_REGEXP', '/https:\/\/downloads\.wordpress\.org\/plugin\/cleantalk-spam-protect\.6\..*?zip/');
 define('AVS_VERSION_SLUG', 'cleantalk-spam-protect');
 define('AVS_ARCHIVE_NAME', 'packed.zip');
-define('AVS_TEMP_ARCHIVE_PATH', __DIR__ . '\temp\\');
-define('AVS_TEMP_DIRECTORY_PATH', __DIR__ . '\temp\versions\\');
+define('AVS_TEMP_ARCHIVE_PATH', wp_get_upload_dir()['path'] . '/avs');
+define('AVS_TEMP_DIRECTORY_PATH', wp_get_upload_dir()['path'] . '/avs/versions');
 define('AVS_DEBUG_DISPLAY', false);
 
-//avs_main();
-add_filter('apbct_stat_report_hook', 'avs_add_selector',  999,  1);
+require_once ('inc/gvs_helper.php');
 
-function avs_add_selector($html)
-{
-    $versions = avs_get_versions();
-    $html = 'OK';
-    $html .= '<form name="avs_select" id="avs_select">';
-    $html .= '<div>';
-    $html .= '<p>Select version you want to install:</p>';
-    $html .= '<select>';
-    $i = 0;
-    foreach ($versions as $version) {
-        $html .= "<option value='$i'>$version</option>";
-    }
-    $html .= '</select>';
-    $html .= '</div>';
-    $html .= '<input type="submit">';
-    $html .= '</form>';
-    return $html;
+if (empty($_POST)) {
+    add_action( 'admin_menu', 'gvs_menu_page', 25  );
 }
 
-function avs_main()
+gvs_main();
+
+function gvs_get_form()
+{
+    $versions = gvs_get_versions();
+    $html = file_get_contents(__DIR__ . '/templates/gvs_form.html');
+    $options = '';
+    foreach ($versions as $version) {
+        $options .= "<option value='$version'>$version</option>";
+    }
+    $html = str_replace('%GVS_OPTIONS_APBCT%', $options, $html);
+
+    echo $html;
+}
+
+
+function gvs_menu_page(){
+
+    add_menu_page(
+        'AVS Page', // тайтл страницы
+        'APBCT Version', // текст ссылки в меню
+        'manage_options', // права пользователя, необходимые для доступа к странице
+        'avs_page', // ярлык страницы
+        'gvs_get_form', // функция, которая выводит содержимое страницы
+        'dashicons-images-alt2', // иконка, в данном случае из Dashicons
+        20 // позиция в меню
+    );
+}
+
+function gvs_main()
 {
     try {
 
-        global $avs_state;
+        global $gvs_state;
 
-        $versions_urls_found = avs_get_versions();
+        if (isset($_POST['gvs_select'])){
 
-        $downloaded_zip_path = avs_download_plugin($versions_urls_found[0], AVS_TEMP_ARCHIVE_PATH);
+            // download selected version gvs_select
+            $gvs_state['downloaded_zip_file_path'] = gvs_download_plugin($_POST['gvs_select'], AVS_TEMP_ARCHIVE_PATH);
 
-        $temp_extracted_path = avs_unpack_zip($downloaded_zip_path, AVS_TEMP_DIRECTORY_PATH);
+            // unpack zip file
+            $gvs_state['new_version_dir'] = gvs_unpack_zip($gvs_state['downloaded_zip_file_path'], AVS_TEMP_DIRECTORY_PATH);
 
-        $avs_state['downloaded_zip_path'] = $downloaded_zip_path;
-        $avs_state['temp_extracted_path'] = $temp_extracted_path;
+            // prepare dirs
 
-        avs_clear_temp_data();
+            $gvs_state['temp_active_plugin_dir'] = AVS_TEMP_DIRECTORY_PATH . '/active';
+            if (is_dir($gvs_state['temp_active_plugin_dir'])) {
+                gvs_delete_folder_recursive($gvs_state['temp_active_plugin_dir']);
+            }
+            $gvs_state['active_plugin_dir'] = WP_PLUGIN_DIR . '/' . AVS_VERSION_SLUG;
+            if (!is_dir($gvs_state['active_plugin_dir'])) {
+                throw new \Exception('Invalid active plugin path');
+            }
+
+            // prepare filesystem
+            if (!gvs_prepare_filesystem()){
+                throw new \Exception('Can not init WordPress filesystem.');
+            }
+
+            // do backup
+            if (!gvs_backup()){
+                throw new \Exception('Can not backup active plugin.');
+            }
+
+            if (!gvs_replace_active_plugin()){
+                throw new \Exception('Can not replace active plugin.');
+            }
+
+            //delete temp files
+            gvs_clear_temp_data();
+
+        }
 
     } catch (\Exception $e) {
-        echo $e;
+        gvs_log($e->getMessage());
     }
 }
 
-function avs_get_versions()
+function gvs_get_versions()
 {
-    avs_log("Api response proceed..");
+    gvs_log("Api response proceed..");
 
     $wp_api_response = @file_get_contents("https://api.wordpress.org/plugins/info/1.0/" . AVS_VERSION_SLUG);
 
@@ -78,9 +118,9 @@ function avs_get_versions()
         throw new \Exception('Empty API response');
     }
 
-    avs_log("Api response successfully got.");
+    gvs_log("Api response successfully got.");
 
-    avs_log("Seek for versions..\n");
+    gvs_log("Seek for versions..\n");
 
     preg_match_all(AVS_VERSIONS_REGEXP, $wp_api_response, $versions_found);
 
@@ -90,16 +130,23 @@ function avs_get_versions()
 
     $versions_found = $versions_found[0];
 
-    avs_log("Versions found:");
+    gvs_log("Versions found:");
 
-    avs_log($versions_found);
+    gvs_log($versions_found);
 
     return $versions_found;
 }
 
-function avs_download_plugin($url, $output_path)
+function gvs_download_plugin($url, $output_path)
 {
-    avs_log( "Downloading content of $url ...");
+    gvs_log( "Downloading content of [$url] to [$output_path] ...");
+
+    if (!is_dir($output_path)) {
+        $result = mkdir($output_path);
+        if (!$result) {
+            throw new \Exception('Can not create temp folder.');
+        }
+    }
 
     $version_content = file_get_contents($url);
 
@@ -107,9 +154,9 @@ function avs_download_plugin($url, $output_path)
         throw new \Exception('Cannot get url content.');
     }
 
-    $downloaded_zip_path = $output_path . AVS_ARCHIVE_NAME;
+    $downloaded_zip_path = $output_path . '/' . AVS_ARCHIVE_NAME;
 
-    avs_log("Writing $downloaded_zip_path ..");
+    gvs_log("Writing $downloaded_zip_path ..");
 
     $result = file_put_contents($downloaded_zip_path, $version_content);
 
@@ -120,14 +167,16 @@ function avs_download_plugin($url, $output_path)
     return $downloaded_zip_path;
 }
 
-function avs_unpack_zip($downloaded_zip_path, $temp_dir_path)
+function gvs_unpack_zip($downloaded_zip_path, $temp_dir_path)
 {
-
     if (!is_dir($temp_dir_path)) {
-        throw new \Exception('Invalid temp dir path');
+        $result = mkdir($temp_dir_path);
+        if (!$result) {
+            throw new \Exception('Invalid temp dir path');
+        }
     }
 
-    avs_log( "Unpacking " . $downloaded_zip_path . " ...");
+    gvs_log( "Unpacking " . $downloaded_zip_path . " ...");
 
     $zip = new ZipArchive();
     $zip->open($downloaded_zip_path);
@@ -136,39 +185,57 @@ function avs_unpack_zip($downloaded_zip_path, $temp_dir_path)
     $zip->extractTo($temp_dir_path);
     $zip->close();
 
-    $extracted_plugin_path = $temp_dir_path . $plugin_folder_name;
+    $extracted_plugin_path = $temp_dir_path . '/' . $plugin_folder_name;
 
     if (!is_dir($extracted_plugin_path)) {
         throw new \Exception('Invalid completed temp path. Roll back..');
     }
 
-    avs_log("Unpacking success: $extracted_plugin_path");
+    gvs_log("Unpacking success: $extracted_plugin_path");
 
     return $extracted_plugin_path;
 }
 
-function avs_clear_temp_data()
+function gvs_clear_temp_data()
 {
-    global $avs_state;
-    avs_log("Delete " . AVS_ARCHIVE_NAME . " ...");
+    global $gvs_state;
+    gvs_log("Delete zip " . AVS_ARCHIVE_NAME . " ...");
 
-    $result = unlink($avs_state['downloaded_zip_path']);
+    $result = unlink($gvs_state['downloaded_zip_file_path']);
 
     if (empty($result)) {
         throw new \Exception('Cannot delete file.');
     }
 
-    avs_log("Deleting success.");
+    gvs_log("Deleting zip success.");
+
+    gvs_log("Delete temp version folder " . $gvs_state['new_version_dir'] . " ...");
+
+    gvs_delete_folder_recursive($gvs_state['new_version_dir']);
+
+    gvs_log("Deleting temp version folder success.");
+
 }
 
-function avs_log($msg)
-{
-    error_log('AVS_DEBUG: ' . var_export($msg,true));
-    if (AVS_DEBUG_DISPLAY) {
-        if (is_string($msg)) {
-            echo $msg . "\n";
-        } else {
-            echo var_export($msg) . "\n";
-        }
-    }
+function gvs_backup(){
+    global $gvs_state;
+    $result = copy_dir($gvs_state['active_plugin_dir'], $gvs_state['temp_active_plugin_dir']);
+    return $result ?: false;
+}
+
+function gvs_replace_active_plugin() {
+    global $gvs_state;
+
+    // enable maintenance mode
+    gvs_maintenance_mode__enable(120);
+
+    // remove active plugin
+    gvs_delete_folder_recursive($gvs_state['active_plugin_dir']);
+
+    // replace active plugin
+    $result = copy_dir($gvs_state['new_version_dir'], $gvs_state['active_plugin_dir']);
+
+    gvs_maintenance_mode__disable();
+
+    return $result ?: false;
 }
